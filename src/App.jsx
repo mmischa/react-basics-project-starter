077a34b8aa53cb53a3d69aadf6b4f28c2d593b34
Mischa <mischa@datdenkikwel.nl>
2024-02-24 23:21:21 +0100

    sorting and click finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,22 @@
 import { useState } from 'react';
 import { RecipePage } from './pages/RecipePage';
 import { RecipeListPage } from './pages/RecipeListPage';
-import { data } from './utils/data';
 
 export const App = () => {
-  const [selectedRecipe, setSelectedRecipe] = useState();
+  const [selectedRecipe, setSelectedRecipe] = useState([]);
 
-  const handleClick = (label) => {
-    setSelectedRecipe(label);
-  };
-
-  const recipes = data.hits;
-
-  console.log(recipes);
-  console.log(selectedRecipe);
-  console.log(data.hits[2].recipe);
+  // const activeRecipe = ( () => selectedRecipe.length  > 0);
+  // console.log(activeRecipe);
 
   return (
     <>
-      {selectedRecipe ? (
+      {selectedRecipe.length > 0 ? (
         // Show if recipe selected
-        <RecipePage recipe={recipes[2].recipe} />
+        <RecipePage recipe={selectedRecipe} />
       ) : (
         // Show if NO recipe selected
         <>
-          <RecipeListPage recipes={recipes} handleClick={handleClick} />
+          <RecipeListPage setSelectedRecipe={setSelectedRecipe} />
         </>
       )}
     </>
