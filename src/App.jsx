cead5afcad5734874cb7cedf888aa3fdd136653c
Mischa <mischa@urban-heroes.nl>
2024-02-28 10:46:17 +0100

    rebuild complet

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
 import { useState } from 'react';
 import { RecipePage } from './pages/RecipePage';
 import { RecipeListPage } from './pages/RecipeListPage';
+import { data } from './utils/data';
+
+const recipesData = data.hits;
 
 export const App = () => {
-  const [selectedRecipe, setSelectedRecipe] = useState([]);
-  
+  // state hook to report a click on a card
+  const [recipeSelected, setRecipeSelected] = useState(false);
+  // filter hook
+  const [filteredRecipes, setFilteredRecipes] = useState(recipesData);
 
-  // const activeRecipe = ( () => selectedRecipe.length  > 0);
-  // console.log(activeRecipe);
+  // One function to filter the array by name --------------------------------------
+  const filterArray = (searchString) => {
+    console.log('Searchstring = ' + searchString);
+    const matches = recipesData.filter((item) =>
+      item.recipe.label.toLowerCase().includes(searchString.toLowerCase())
+    );
+    setFilteredRecipes(matches);
+   // console.log(matches);
+  };
 
   return (
     <>
-      {selectedRecipe.length > 0 ? (
+      {recipeSelected === true ? (
         // Show if recipe selected
-        <RecipePage recipe={selectedRecipe} />
+        <RecipePage recipe={filteredRecipes} />
       ) : (
         // Show if NO recipe selected
         <>
-          <RecipeListPage setSelectedRecipe={setSelectedRecipe} />
+          <RecipeListPage
+            filterArray={filterArray}
+            recipes={filteredRecipes}
+            setRecipeSelected={setRecipeSelected}
+          />
         </>
       )}
     </>
